c454f85d3e79a5546942c041e1f109af6be43e3b
Jorge Carlos Quevedo <jorgecarlosquevedo@gmail.com>
2025-06-30 11:59:25 -0700

    simple html

---
+++ script.js
@@ -0,0 +1,61 @@
+const totalSlides = 41
+let currentSlide = 1
+
+$(document).ready(function () {
+  // Add all slide images
+  for (let i = 1; i <= totalSlides; i++) {
+    const $img = $('<img>')
+      .attr('src', `/slides/slide${i}_small.jpg`)
+      .attr('data-hires', `/slides/slide${i}_large.jpg`)
+      .addClass('slide')
+      .attr('id', `slide${i}`)
+
+    if (i === 1) $img.addClass('active')
+    $('#slideshow').append($img)
+  }
+
+  preloadAdjacent(currentSlide)
+
+  $('.left').on('click', () => {
+    currentSlide = currentSlide > 1 ? currentSlide - 1 : totalSlides
+    showSlide(currentSlide)
+  })
+
+  $('.right').on('click', () => {
+    currentSlide = currentSlide < totalSlides ? currentSlide + 1 : 1
+    showSlide(currentSlide)
+  })
+
+  $('#reset').on('click', function (e) {
+    e.preventDefault()
+    currentSlide = 1
+    showSlide(currentSlide)
+  })
+})
+
+function showSlide(n) {
+  $('.slide').removeClass('active')
+  $(`#slide${n}`).addClass('active')
+  swapToHighRes(n)
+  preloadAdjacent(n)
+}
+
+function swapToHighRes(n) {
+  const $img = $(`#slide${n}`)
+  if (!$img.hasClass('hires')) {
+    $img.attr('src', $img.data('hires'))
+    $img.addClass('hires')
+  }
+}
+
+function preloadAdjacent(n) {
+  for (let offset = -2; offset <= 2; offset++) {
+    let i = ((n - 1 + offset + totalSlides) % totalSlides) + 1
+    const $img = $(`#slide${i}`)
+    if ($img.length && !$img.hasClass('hires')) {
+      const hiresSrc = $img.data('hires')
+      const preloader = new Image()
+      preloader.src = hiresSrc
+    }
+  }
+}
